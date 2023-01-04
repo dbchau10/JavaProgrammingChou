3f684257b0b0078577529166950b7c6b6acb6df8
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2023-01-04 19:38:20 +0700

    unfriend refactor code

--- src/components/FriendCom.java
+++ src/components/FriendCom.java
@@ -11,9 +11,9 @@ import javax.swing.JSeparator;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
-<<<<<<<HEAD
-import views.AllTab;=======
-import views.FriendFunction;>>>>>>>f0d78ba0e618f82c9a855a70e7f7166862b34360
+
+import views.AllTab;
+import views.FriendFunction;
 import views.User;
 
 import java.awt.Font;
@@ -50,13 +50,12 @@ public class FriendCom {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-		public JPanel initialize(String username, String button) {
-		
-		
-		//friend.setBounds(0, 0, 404, 100);
+	public JPanel initialize(String username, String button) {
+
+		// friend.setBounds(0, 0, 404, 100);
 		friend.setPreferredSize(new Dimension(404, 50));
 		friend.setLayout(null);
-		
+
 		JLabel lblUsername = new JLabel(username);
 		lblUsername.setFont(new Font("Arial", Font.PLAIN, 13));
 		lblUsername.setBounds(29, 10, 140, 43);
@@ -65,7 +64,7 @@ public class FriendCom {
 		btnMess.setFont(new Font("Arial", Font.PLAIN, 10));
 		btnMess.setBounds(286, 20, 71, 17);
 		friend.add(btnMess);
-		
+
 		JButton btn = new JButton(button);
 		btn.setForeground(new Color(255, 255, 255));
 		btn.setBackground(new Color(241, 84, 7));
@@ -74,85 +73,84 @@ public class FriendCom {
 		friend.add(btn);
 		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (btn.getText()=="Hủy")
-				{
+				if (btn.getText() == "Hủy") {
 					FriendFunction del = new FriendFunction(conn, you);
 					del.DeleteFriend(other.getUsername());
 
-					/*try {
-						String sql = new String("DELETE FROM friendlist WHERE user_id1=? and user_id2=?");
-						conn.setAutoCommit(false);
+					/*
+					 * try {
+					 * String sql = new
+					 * String("DELETE FROM friendlist WHERE user_id1=? and user_id2=?");
+					 * conn.setAutoCommit(false);
+					 * stm = conn.prepareStatement(sql);
+					 * stm.setInt(1, you.getID());
+					 * stm.setInt(2, other.getID());
+					 * stm.executeUpdate();
+					 * conn.commit();
+					 * 
+					 * sql = new String("DELETE FROM friendlist WHERE user_id1=? and user_id2=?");
+					 * conn.setAutoCommit(false);
+					 * stm = conn.prepareStatement(sql);
+					 * stm.setInt(2, you.getID());
+					 * stm.setInt(1, other.getID());
+					 * stm.executeUpdate();
+					 * conn.commit();
+					 * 
+					 * 
+					 * 
+					 * } catch(SQLException ex)
+					 * {
+					 * try {
+					 * conn.rollback();
+					 * }
+					 * catch(SQLException e1) {
+					 * e1.printStackTrace();
+					 * }
+					 * }
+					 */
+					PreparedStatement stm =null;
+					ResultSet res = null;
+					List<User> FriendList = new ArrayList<>();
+					try {
+						String sql = "SELECT * FROM USERS US LEFT JOIN FRIENDLIST FR ON US.USER_ID = FR.USER_ID1 WHERE FR.USER_ID2 IN (SELECT U.USER_ID FROM USERS U WHERE U.USER_NAME=?)";
 						stm = conn.prepareStatement(sql);
-						stm.setInt(1, you.getID());
-						stm.setInt(2, other.getID());
-						stm.executeUpdate();
-						conn.commit();
-						
-						sql = new String("DELETE FROM friendlist WHERE user_id1=? and user_id2=?");
-						conn.setAutoCommit(false);
-						stm = conn.prepareStatement(sql);
-						stm.setInt(2, you.getID());
-						stm.setInt(1, other.getID());
-						stm.executeUpdate();
-						conn.commit();
-						
-						
-						
-					} catch(SQLException ex)
-					{
-						try {
-							conn.rollback();
+						stm.setString(1, you.getUsername());
+						res = stm.executeQuery();
+						while (res.next()) {
+
+							Integer id = res.getInt("user_id");
+							String username = res.getString("user_name");
+							String hoten = res.getString("user_hoten");
+							String dob = res.getString("user_ngaysinh");
+							String email = res.getString("user_email");
+							String address = res.getString("user_diachi");
+							User user = new User(id, username, hoten, dob, email, address);
+							System.out.print(user.getUsername());
+							FriendList.add(user);
+
 						}
-						catch(SQLException e1) {
-							e1.printStackTrace();
-						}
+					} catch (SQLException ex) {
+						ex.printStackTrace();
 					}
-					*/
-					finally {
-						ResultSet res = null;
-						List<User>FriendList = new ArrayList<>();
-						try {
-							String sql = "SELECT * FROM USERS US LEFT JOIN FRIENDLIST FR ON US.USER_ID = FR.USER_ID1 WHERE FR.USER_ID2 IN (SELECT U.USER_ID FROM USERS U WHERE U.USER_NAME=?)";
-							stm = conn.prepareStatement(sql);
-							stm.setString(1, you.getUsername());
-							res = stm.executeQuery();
-							while(res.next()) {
-								
-								Integer id = res.getInt("user_id");
-								String username = res.getString("user_name");
-								String hoten = res.getString("user_hoten");
-								String dob = res.getString("user_ngaysinh");
-								String email = res.getString("user_email");
-								String address = res.getString("user_diachi");
-								User user = new User(id,username,hoten,dob,email,address);
-								System.out.print(user.getUsername());
-								FriendList.add(user);
-								
-							}
-						} catch(SQLException ex)
-						{
-							ex.printStackTrace();
-						}
-						
-						AllTab.listAll.removeAll();
-						AllTab.listAll.revalidate();
-						AllTab.listAll.repaint();
-						
-						for (int i = 0 ; i<FriendList.size(); i++)
-						{
-							FriendCom fr = new FriendCom(conn,you,FriendList.get(i));
-							AllTab.listAll.add(fr.initialize(FriendList.get(i).getUsername(),"Hủy"));
-							JSeparator separator = new JSeparator();
-							separator.setBounds(10, 33, 353, 2);
-							
-							AllTab.listAll.add(separator);
-						}
+
+					AllTab.listAll.removeAll();
+					AllTab.listAll.revalidate();
+					AllTab.listAll.repaint();
+
+					for (int i = 0; i < FriendList.size(); i++) {
+						FriendCom fr = new FriendCom(conn, you, FriendList.get(i));
+						AllTab.listAll.add(fr.initialize(FriendList.get(i).getUsername(), "Hủy"));
+						JSeparator separator = new JSeparator();
+						separator.setBounds(10, 33, 353, 2);
+
+						AllTab.listAll.add(separator);
 					}
+
 				}
 			}
 		});
 		return friend;
-		
+
 	}
 
 	private void redispatchToParent(ActionEvent e) {
